7df3093fbbeb5758acc958db6d63e27476bc3fbd
sufferpod <sadra759@gmail.com>
2024-05-28 14:10:54 +0330

    mob

--- tam2.cpp
+++ tam2.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+
 using namespace std;
 
 class Date
 {
 public:
     int year, month, day;
-    void print() { cout << year << '/' << month << '/' << day << endl; }
+    void print() { cout << setfill('0') << setw(2) << year << '/' << setfill('0') << setw(2) << month << '/' << setfill('0') << setw(2) << day << endl; }
     Date(int y = 0, int m = 0, int d = 0) : year(y), month(m), day(d) {}
 };
 
@@ -15,7 +17,7 @@ class Author
 public:
     string firstName, lastName;
     void print() { cout << "Author: " << firstName << ' ' << lastName << endl; }
-    Author(string fn = "none", string ln = "none") : firstName(fn), lastName(ln) {}
+    Author(string fn, string ln) : firstName(fn), lastName(ln) {}
 };
 
 class Article
@@ -27,8 +29,8 @@ protected:
 
 public:
     virtual void print() = 0;
-    Article(string ti = "none", string bo = "none", Date da = {0, 0, 0}, Author au = {"none", "none"}) : title(ti),
-                                                                                                         body(bo), date(da), author(au) {}
+    Article(string ti, string bo, Date da, Author au) : title(ti),
+                                                        body(bo), date(da), author(au) {}
 };
 
 class Lecture : public Article
@@ -37,7 +39,7 @@ private:
     string jornal;
 
 public:
-    Lecture(string jo = "none", string ti = "none", string bo = "none", Date da = {0, 0, 0}, Author au = {"none", "none"}) : Article(), jornal(jo) {}
+    Lecture(string jo, string ti, string bo, Date da, Author au) : Article(ti, bo, da, au), jornal(jo) {}
     void print()
     {
         cout << "jornal title: " << jornal << '\n'
@@ -55,7 +57,7 @@ private:
     string affiliation;
 
 public:
-    Paper(string af = "khansar") : Article(), affiliation(af) {}
+    Paper(string af, string ti, string bo, Date da, Author au) : Article(ti, bo, da, au), affiliation(af) {}
     void print()
     {
         cout << "affiliation: " << affiliation << '\n'
@@ -69,8 +71,8 @@ public:
 int main()
 {
     Article *X, *Y, *Z;
-    Y = new Lecture("Khanbook", "sadra", "body", {3, 3, 3}, {"mobina", "miri"});
-    Z = new Paper();
+    Y = new Lecture("Khanbook", "sadra", "body", Date(3, 3, 3), Author("mobina", "miri"));
+    Z = new Paper("Khansar", "sadra", "body", Date(3, 3, 3), Author("mobina", "miri"));
     Y->print();
     Z->print();
     return 0;
